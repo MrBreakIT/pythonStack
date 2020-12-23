ab9d35b122a0f8b17676302d890475af4e695fb0
MrBreakIT <johnpike1022@gmail.com>
2020-12-23 11:24:14 -0600

    added sportsORM

---
+++ 05_django/02_djangoORM/sports_orm/leagues/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .models import League, Team, Player
+
+from . import team_maker
+
+def index(request):
+	context = {
+		"leagues": League.objects.all(),
+		"teams": Team.objects.all(),
+		"players": Player.objects.all(),
+		# "leagues": League.objects.filter(sport="baseball"),
+	}
+	return render(request, "leagues/index.html", context)
+
+def make_data(request):
+	team_maker.gen_leagues(10)
+	team_maker.gen_teams(50)
+	team_maker.gen_players(200)
+
+	return redirect("index")
